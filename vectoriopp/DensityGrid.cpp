fbe75ac2eea19235b270c6b3d33167ee34ecc68d
RainbowCats <sycamore361@hotmail.com>
2022-12-11 18:18:15 -0800

    Add missing files

---
+++ vectoriopp/DensityGrid.cpp
@@ -0,0 +1,94 @@
+#include "DensityGrid.h"
+#include <math.h>
+#include <future>
+#include "TunableParameters.h"
+
+#define CHECK_COORD(c) if (abs(c) > ROOM_W/2.0f) { return; }
+// Make sure points are in room scope
+#define COORD2GRID(c)  (int)((c + ROOM_W/2.0f) * (GRID_NUM/(float)ROOM_W))
+// Returns the indices in to the grid array corresponding to a given pos
+void DensityGrid::findGridLocation(glm::vec3 pos, int& x, int& y, int& z) {
+	 CHECK_COORD(pos[0]); CHECK_COORD(pos[1]); CHECK_COORD(pos[2]);
+	 x = COORD2GRID(pos[0]);
+	 y = COORD2GRID(pos[1]);
+	 z = COORD2GRID(pos[2]);
+}
+
+// Record a stamp at given grid indices
+void DensityGrid::stamp(int x, int y, int z, int expansion, float maxStamp) {
+	for (int xi = x-expansion; xi < x+expansion + 1 ; xi++) {
+		if (xi < 0 || xi >= GRID_NUM) {
+			continue;
+		}
+		for (int yi = y - expansion; yi < y + expansion + 1; yi++) {
+			if (yi < 0 || yi >= GRID_NUM) {
+				continue;
+			}
+			for (int zi = z - expansion; zi < z + expansion + 1; zi++) {
+				if (zi < 0 || zi >= GRID_NUM) {
+					continue;
+				}
+				float stampRaw = maxStamp - (float)(pow(abs(x - xi) * 1.2, 1.5) + pow(abs(y - yi) * 1.2, 1.5) + pow(abs(z - zi) * 1.2, 1.5));
+				float stamp = fmax(0, stampRaw);
+				grid[xi][yi][zi] += stamp;
+			}
+		}
+	}
+}
+
+void DensityGrid::recordParticleAt(glm::vec3 pos) {
+	if (mDontRecord == true) {
+		return;
+	}
+	int x, y, z;
+	findGridLocation(pos, x, y, z);
+	stamp(x, y, z);
+}
+
+void DensityGrid::doneProfiling() {
+	mDontRecord = false;
+}
+
+#define G2C(g) (((g/((float)GRID_NUM))*ROOM_W)-ROOM_W/2.0f)
+std::future<std::vector<DensityGrid::Entry>> DensityGrid::profile() {
+	mDontRecord = true;
+	return std::async(std::launch::async, [&]() {
+		int maxLocations = 100000;
+		//int currLocations = 0;
+		std::vector<Entry> profileLocations;
+		for (int yi = 0; yi < GRID_NUM; yi++) {
+			for (int zi = 0; zi < GRID_NUM; zi++) {
+				bool start = false;
+				float value = 0.0f;
+				for (int xi = 0; xi < GRID_NUM; xi++) {
+					if (start == false && grid[xi][yi][zi] > THRESHOLD) {
+						start = true;
+						profileLocations.push_back(Entry(G2C(xi), G2C(yi), G2C(zi)));
+						printf("entering density value is %f\n", grid[xi][yi][zi]);
+						//currLocations++;
+						if (profileLocations.size() > maxLocations) {
+							return profileLocations;
+						}
+						continue;
+					}
+
+					if (start == true && grid[xi][yi][zi] <= THRESHOLD) {
+						start = false;
+						profileLocations.push_back(Entry(G2C(xi), G2C(yi), G2C(zi)));
+						printf("exiting density value is %f\n", grid[xi][yi][zi]);
+						if (profileLocations.size() > maxLocations) {
+							return profileLocations;
+						}
+						continue;
+					}
+				}
+			}
+		}
+		printf("Profiling thread complete");
+		return profileLocations;
+		});
+}
+
+double DensityGrid::evaluate(glm::vec3 pos) {
+	return 0.0f;
+}
---
+++ vectoriopp/DensityGrid.h
@@ -0,0 +1,50 @@
+#pragma once 
+#include <glm/glm.hpp>
+#include <GL/glew.h>
+#include <vector>
+#include <future>
+#include "TunableParameters.h"
+#include <unordered_set>
+
+class DensityGrid {
+public:
+	DensityGrid() = default;
+	~DensityGrid() = default;
+
+	struct Entry {
+		float x = 0, y = 0, z = 0;
+		Entry(float xin, float yin, float zin) : x(xin), y(yin), z(zin) {}
+	};
+
+	const float ROOM_W = 12.0f;
+	const float ROOM_D = 6.0f;
+	const float ROOM_H = 6.0f;
+	
+	const float GRID_W = ROOM_W / GRID_NUM;
+	const float GRID_D = ROOM_D / GRID_NUM;
+	const float GRID_H = ROOM_H / GRID_NUM;
+
+    void recordParticleAt(glm::vec3 pos); // do stamp and findGridLocation
+	double evaluate(glm::vec3 pos);
+	std::future<std::vector<Entry>> profile();
+
+	void doneProfiling(); // turn off recordParticleAt when true
+
+private:
+	GLfloat grid[GRID_NUM][GRID_NUM][GRID_NUM];
+	void stamp(int grid_x, int grid_y, int grid_z, int spread = STAMP_SPREAD, float maxStamp = MAX_STAMP);
+	void findGridLocation(glm::vec3 pos, int& x, int& y, int& z);
+	bool mDontRecord = false;
+};
+
+/*
+class BodyField : public DensityGrid {
+	BodyField(glm::vec3 pos);
+	~BodyField();
+};
+
+class JointField : public DensityGrid {
+	JointField(glm::vec3 pos);
+	~JointField();
+};
+*/
